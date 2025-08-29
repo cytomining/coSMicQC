d9ff46432bac44cf5c71aad5f23fabff55754f5e
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2025-08-29 14:59:54 -0600

    add manuscript figures for review

---
+++ docs/manuscripts/cosmicqc_paper_v1/figure_5/run_single_cell_qc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# activate the cosmicqc poetry environment
+poetry shell
+
+# Define the path to the parent folder to generate list of plate IDs
+PARENT_FOLDER="/media/NVME_4TB/LINCS_cytotable_output/data/"
+
+# Create an array of folder names (excluding files)
+plates=($(find "$PARENT_FOLDER" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
+
+# Print the count of folders
+echo "Number of plates found: ${#plates[@]}"
+
+# Using papermill, run single cell quality control on all plates
+for plate in "${plates[@]}"; do
+    papermill \
+    2.single_cell_qc.ipynb \
+    2.single_cell_qc.ipynb \
+    -p plate_id $plate
+done
+
+echo "Single cell QC completed for all plates."
