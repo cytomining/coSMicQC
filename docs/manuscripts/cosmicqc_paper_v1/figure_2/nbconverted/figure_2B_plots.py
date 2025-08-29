9f53cb07c29539bb2e6690ce670d6bcca060dd9c
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2025-08-29 15:35:21 -0600

    add nbconverted files

---
+++ docs/manuscripts/cosmicqc_paper_v1/figure_2/nbconverted/figure_2B_plots.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Generate figures related to Figure 3
+
+# In[1]:
+
+
+from pathlib import Path
+
+import pandas as pd
+from plotnine import *  # noqa: F403
+from plotnine.options import set_option
+
+from cosmicqc import identify_outliers
+
+
+# ## Set paths and variables
+
+# In[2]:
+
+
+# Set output directory for figures
+output_dir = Path("./figures")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Load the data to generate the plots
+example_df = pd.read_parquet(
+    Path(
+        "/media/18tbdrive/1.Github_Repositories/pediatric_cancer_atlas_profiling/3.preprocessing_features/data/converted_profiles/Round_2_data/BR00145816_converted.parquet"
+    )
+)
+
+# metadata columns to include in output data frame
+metadata_columns = [
+    "Image_Metadata_Plate",
+    "Image_Metadata_Well",
+    "Image_Metadata_Site",
+    "Metadata_Nuclei_Location_Center_X",
+    "Metadata_Nuclei_Location_Center_Y",
+    "Image_FileName_OrigDNA",
+    "Image_FileName_OrigAGP",
+    "Image_PathName_OrigDNA",
+    "Image_PathName_OrigAGP",
+    "Nuclei_AreaShape_BoundingBoxMaximum_X",
+    "Nuclei_AreaShape_BoundingBoxMaximum_Y",
+    "Nuclei_AreaShape_BoundingBoxMinimum_X",
+    "Nuclei_AreaShape_BoundingBoxMinimum_Y",
+]
+
+# Define the QC features
+qc_features = [
+    "Nuclei_Intensity_IntegratedIntensity_CorrDNA",
+    "Nuclei_AreaShape_Solidity",
+    "Nuclei_Intensity_MassDisplacement_CorrDNA",
+]
+
+# Filter plate_df to only include metadata columns and QC features
+filtered_plate_df = example_df[metadata_columns + qc_features]
+
+print(example_df.shape)
+example_df.head()
+
+
+# In[3]:
+
+
+# Find large nuclei outliers for the current plate
+identify_nuclei_clustered_outliers = identify_outliers(
+    df=filtered_plate_df,
+    feature_thresholds={
+        # Set very low as to detect all instances of clustering nuclei
+        "Nuclei_Intensity_MassDisplacement_CorrDNA": 0.05,
+        # Set higher than displacement to avoid false positives
+        "Nuclei_Intensity_IntegratedIntensity_CorrDNA": 1.5,
+    },
+    include_threshold_scores=True,
+)
+
+pd.DataFrame(identify_nuclei_clustered_outliers).head()
+
+
+# In[4]:
+
+
+# Rename for easier access
+df = identify_nuclei_clustered_outliers.rename(
+    columns={
+        "cqc.custom.Z_Score.Nuclei_Intensity_IntegratedIntensity_CorrDNA": "zscore_intensity",
+        "cqc.custom.Z_Score.Nuclei_Intensity_MassDisplacement_CorrDNA": "zscore_displacement",
+        "cqc.custom.is_outlier": "is_outlier",
+    }
+)
+
+# Make sure 'is_outlier' is a string or category (for coloring)
+df["is_outlier"] = df["is_outlier"].map({True: "Outlier", False: "Not Outlier"})
+
+# Set the figure size
+height = 5
+width = 8
+set_option("figure_size", (width, height))
+
+# Plot
+p = (
+    ggplot(df, aes(x="zscore_intensity", y="zscore_displacement", color="is_outlier"))
+    + geom_point(alpha=0.3, size=2)
+    + geom_vline(xintercept=1.5, linetype="--", color="#800080", size=1.0)
+    + geom_hline(yintercept=0.05, linetype="--", color="#800080", size=1.0)
+    + scale_color_manual(values={"Outlier": "#A658A6", "Not Outlier": "#5C8F5C"})
+    + labs(
+        x="Z-score (nuclei intensity)",
+        y="Z-score\n(nuclei mass displacement)",
+        color="Outlier status",
+    )
+    + ggtitle("Over-segmented nuclei detection")
+    + theme_light()
+    + theme(
+        legend_title=element_text(size=16),
+        legend_text=element_text(size=14),
+        legend_position=(0.97, 0.97),
+        axis_title=element_text(size=18),
+        axis_text=element_text(size=15),
+        plot_title=element_text(size=18),
+        axis_title_y=element_text(
+            angle=90,
+            vjust=0.5,
+            ha="center",
+            size=18,
+        ),
+        plot_margin_left=0.03,
+    )
+)
+# Save the plot
+p.save(
+    output_dir / "over_segmented_nuclei_plot.png", dpi=600, width=width, height=height
+)
+
+p.show()
+
+
+# In[5]:
+
+
+# Find large nuclei outliers for the current plate
+identify_poor_nuclei_shape_outliers = identify_outliers(
+    df=filtered_plate_df,
+    # Set at this point where it looks like it starts to detect good quality nuclei
+    feature_thresholds={
+        "Nuclei_AreaShape_Solidity": -1.6,
+    },
+    include_threshold_scores=True,
+)
+
+pd.DataFrame(identify_poor_nuclei_shape_outliers).head()
+
+
+# In[7]:
+
+
+# Rename for easier access
+df = identify_poor_nuclei_shape_outliers.rename(
+    columns={
+        "cqc.custom.Z_Score.Nuclei_AreaShape_Solidity": "zscore_solidity",
+        "cqc.custom.is_outlier": "is_outlier",
+    }
+)
+
+# Make sure 'is_outlier' is a string or category (for coloring)
+df["is_outlier"] = df["is_outlier"].map({True: "Outlier", False: "Not Outlier"})
+
+# Set the figure size
+set_option("figure_size", (width, height))
+
+p = (
+    ggplot(df, aes(x="zscore_solidity", fill="is_outlier"))
+    + geom_histogram(position="stack", bins=45, color="black")
+    + scale_fill_manual(values={"Not Outlier": "#5C8F5C", "Outlier": "#A658A6"})
+    + geom_vline(
+        xintercept=df.loc[df["is_outlier"] == "Outlier", "zscore_solidity"].max(),
+        linetype="dashed",
+        color="#800080",
+        size=1.0,
+    )
+    + labs(x="Z-score (nuclei solidity)", y="Single-cell count", fill="Outlier Status")
+    + ggtitle("Poorly-segmented nuclei detection")
+    + theme_light()
+    + theme(
+        legend_title=element_text(size=16),
+        legend_text=element_text(size=14),
+        legend_position=(0.07, 0.97),
+        axis_title=element_text(size=18),
+        axis_text=element_text(size=15),
+        plot_title=element_text(
+            size=18,
+        ),
+    )
+)
+
+# Save the plot
+p.save(
+    output_dir / "pooly_segmented_nuclei_plot.png", dpi=600, width=width, height=height
+)
+
+p.show()
+
---
+++ docs/manuscripts/cosmicqc_paper_v1/figure_3/nbconverted/panelD_UMAP_with_QC.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Generate UMAP embeddings from data after QC and plot
+
+# In[1]:
+
+
+from pathlib import Path
+
+import pandas as pd
+from plotnine import *  # noqa: F403
+from plotnine.options import set_option
+
+import umap
+
+from pycytominer.cyto_utils import infer_cp_features
+
+
+# In[2]:
+
+
+# Set constants
+umap_random_seed = 0
+umap_n_components = 2
+
+# Set output directory
+output_dir = Path("./figures")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Load in no QC normalized dataframe for CFReT example plate
+QC_df = pd.read_parquet(
+    Path(
+        "/media/18tbdrive/1.Github_Repositories/cellpainting_predicts_cardiac_fibrosis/3.process_cfret_features/data/single_cell_profiles/localhost230405150001_sc_feature_selected.parquet"
+    )
+)
+
+# Drop any NaN rows from columns that are not metadata (contain Metadata_ prefix)
+QC_df = QC_df.dropna(
+    subset=[col for col in QC_df.columns if not col.startswith("Metadata_")]
+).reset_index(drop=True)
+
+# Create new column for treatment cell type ID for each unique combo
+QC_df["Metadata_Treatment_CellType_ID"] = (
+    QC_df["Metadata_treatment"] + "_" + QC_df["Metadata_cell_type"]
+)
+
+# Print shape of the DataFrame
+print(QC_df.shape)
+QC_df.head()
+
+
+# In[3]:
+
+
+# Process cp_df to separate features and metadata
+cp_features = infer_cp_features(QC_df)
+meta_features = infer_cp_features(QC_df, metadata=True)
+
+# Make sure to reinitialize UMAP instance per plate
+umap_fit = umap.UMAP(
+    random_state=umap_random_seed, n_components=umap_n_components, n_jobs=1
+)
+
+# Fit UMAP and convert to pandas DataFrame
+embeddings = pd.DataFrame(
+    umap_fit.fit_transform(QC_df.loc[:, cp_features]),
+    columns=[f"UMAP{x}" for x in range(0, umap_n_components)],
+)
+print(f"{embeddings.shape} UMAP embeddings generated")
+
+# Combine with metadata
+cp_umap_with_metadata_df = pd.concat([QC_df.loc[:, meta_features], embeddings], axis=1)
+
+
+# In[4]:
+
+
+# Set the figure size
+height = 5
+width = 7
+set_option("figure_size", (width, height))
+
+# Plot with custom color palette
+p = (
+    ggplot(
+        cp_umap_with_metadata_df,
+        aes(x="UMAP0", y="UMAP1"),
+    )
+    + labs(
+        color="Cell type\nand treatment",
+    )
+    + geom_point(color="steelblue", alpha=0.2, size=2)
+    + facet_wrap(
+        "Metadata_Treatment_CellType_ID",
+        ncol=2,
+        scales="fixed",
+    )
+    + theme_bw()
+    + theme(
+        axis_title=element_text(size=13),
+        axis_text=element_text(size=11),
+        plot_title=element_text(
+            size=14,
+        ),
+    )
+    + scale_color_brewer(type="qual", palette="Dark2")  # Change palette as needed
+)
+
+p.show()
+
+
+# In[5]:
+
+
+# Set the figure size
+height = 8
+width = 8
+set_option("figure_size", (width, height))
+
+# Plot with custom color palette
+p = (
+    ggplot(
+        cp_umap_with_metadata_df,
+        aes(x="UMAP0", y="UMAP1", color="Metadata_Treatment_CellType_ID"),
+    )
+    + labs(
+        color="Cell type\nand treatment",
+    )
+    + geom_point(alpha=0.2, size=2)
+    + theme_bw()
+    + theme(
+        axis_title=element_text(size=20),
+        axis_text=element_text(size=16),
+        legend_position="bottom",
+        legend_title=element_text(size=16),
+        legend_text=element_text(size=14),
+    )
+    + scale_color_brewer(type="qual", palette="Dark2")  # Change palette as needed
+)
+
+# Save the plot
+p.save(output_dir / "facet_umap_with_QC_plot.png", dpi=400, width=width, height=height)
+
+p.show()
+
